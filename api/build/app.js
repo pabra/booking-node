d8f1364d6b6f053418d0b7c85aaa09e776a332b6
pabra <pabra@netzfruech.de>
2016-08-06 19:03:42 +0200

    inspect node module system

--- api/build/app.js
+++ api/build/app.js
@@ -18,3 +18,24 @@ app.post('/auth/:email', routes.auth);
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+// function logModules() {
+//     var tracker = {},
+//         printModule = function(m, i) {
+//         var indent = '',
+//             myId = m.id.replace('/home/pepp/Private/git/booking-node/api/build/', ''),
+//             x;
+//         if (tracker[myId]) {tracker[myId] += 1;} else {tracker[myId] = 1;}
+//         if ('undefined' === typeof i) {i = 0;} else {i += 1;}
+//         for (x=0; x<=i; x++) indent += '  ';
+//         console.log(i + indent, myId);
+//         if (m.children.length) {
+//             for (x=0; x<m.children.length; x++) {
+//                 printModule(m.children[x], i);
+//             }
+//         }
+//     };
+//     printModule(require.main);
+//     console.log(tracker);
+// }
+// setTimeout(logModules, 1000);
