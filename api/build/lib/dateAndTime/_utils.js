fe12b9e6c0f1e922d1f10e74ab621c57111568fa
pabra <pabra@netzfruech.de>
2016-08-09 22:45:31 +0200

    add dateAndTime utils

---
+++ api/build/lib/dateAndTime/_utils.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var errors = require('../errors'),
+    ValueError = errors.ValueError,
+    utils = require('../utils'),
+    mkDate, setFirstDayOfMonth, setLastDayOfMonth;
+
+
+/**
+ * mkDateFn - get new Date object
+ *
+ * @param  {number} year    - year as int
+ * @param  {number} month   - month as int
+ * @param  {number} day     - day as int
+ * @return {object}         - new Date() object
+ */
+mkDate = function mkDateFn (year, month, day) {
+    var newDate;
+
+    if (!utils.isInt(year) || !utils.isInt(month) || !utils.isInt(day)) {
+        throw new ValueError(`year, month, day must be integer: ${year}, ${month}, ${day}`);
+    }
+
+    newDate = new Date(0);
+    newDate.setUTCFullYear(year);
+    newDate.setUTCMonth(month - 1);
+    newDate.setUTCDate(day);
+
+    if (newDate.getUTCFullYear() !== year
+            || newDate.getUTCMonth() !== month - 1
+            || newDate.getUTCDate() !== day) {
+
+        throw new ValueError(`date "${newDate}" does not match passed year, month, day; ${year}, ${month}, ${day}`);
+    }
+
+    return newDate;
+};
+
+setFirstDayOfMonth = function setFirstDayOfMonthFn (dObj) {
+    if (!dObj instanceof Date) {
+        throw new ValueError(`not an instance of Date: ${dObj}`);
+    }
+
+    dObj.setUTCDate(1);
+    dObj.setUTCHours(0);
+    dObj.setUTCMinutes(0);
+    dObj.setUTCSeconds(0);
+    dObj.setUTCMilliseconds(0);
+
+    return dObj;
+};
+
+setLastDayOfMonth = function setLastDayOfMonthFn (dObj) {
+    var fullDay = 24 * 60 * 60 * 1000;
+
+    if (!dObj instanceof Date) {
+        throw new ValueError(`not an instance of Date: ${dObj}`);
+    }
+
+    dObj = setFirstDayOfMonth(dObj);
+    // add 32 days
+    dObj = setFirstDayOfMonth(new Date(dObj.getTime() + 32 * fullDay));
+    // substract one day
+    dObj = new Date(dObj.getTime() - fullDay);
+
+    return dObj;
+};
+
+exports.mkDate = mkDate;
+exports.setFirstDayOfMonth = setFirstDayOfMonth;
+exports.setLastDayOfMonth = setLastDayOfMonth;
--- api/build/lib/dateAndTime/index.js
+++ api/build/lib/dateAndTime/index.js
@@ -1,4 +1,9 @@
 "use strict";
 
+var _utils = require('./_utils');
+
 exports.ensureValidYearMonth = require('./ensureValidYearMonth');
 exports.ensureValidIsoDate = require('./ensureValidIsoDate');
+exports.mkDate = _utils.mkDate;
+exports.setFirstDayOfMonth = _utils.setFirstDayOfMonth;
+exports.setLastDayOfMonth = _utils.setLastDayOfMonth;
