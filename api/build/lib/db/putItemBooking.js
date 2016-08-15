b21a54b6ae6c8cfdbfd77a45db091c3899f3f37b
pabra <pabra@netzfruech.de>
2016-08-15 23:20:04 +0200

    use real random uid to insert booking request

--- api/build/lib/db/putItemBooking.js
+++ api/build/lib/db/putItemBooking.js
@@ -1,9 +1,11 @@
 "use strict";
 
 var co = require('co'),
+    uid = require('../uid'),
     logger = require('../logger'),
     transactionPromise = require('./_transactionPromise'),
-    transactionQueryPromise = require('./_transactionQueryPromise');
+    transactionQueryPromise = require('./_transactionQueryPromise'),
+    maxTries = 25;
 
 
 module.exports = function putItemBookingFn (itemUid, fromDate, toDate) {
@@ -11,7 +13,7 @@ module.exports = function putItemBookingFn (itemUid, fromDate, toDate) {
 
     logger.debug('define txFn');
     txFn = co.wrap(function * (conn) {
-        var q, result;
+        var q, result, newReqUid, insErr, i;
 
         q = 'SELECT NOW()';
         logger.debug('in txFn q1:', q);
@@ -25,7 +27,19 @@ module.exports = function putItemBookingFn (itemUid, fromDate, toDate) {
             (?, 2, ?, ?, NOW())
         `;
         logger.debug('in txFn q2:', q);
-        result = yield transactionQueryPromise(conn, q, ['requ09', fromDate, toDate]);
+        i = 0;
+        while (i++ === 0 || (insErr && 'ER_DUP_ENTRY' === insErr.code)) {
+            if (i >= maxTries) throw new Error(`too many retries (${i}) for inserting new request`);
+
+            newReqUid = yield uid.getStrongUid(6);
+
+            try {
+                result = yield transactionQueryPromise(conn, q, [newReqUid, fromDate, toDate]);
+            } catch (err) {
+                if ('ER_DUP_ENTRY' === err.code) insErr = err;
+                else throw err;
+            }
+        }
         logger.debug('in txFn res2:', result);
         // throw new Error('test');
 
