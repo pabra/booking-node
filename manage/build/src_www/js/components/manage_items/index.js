c9c5dbe01eb6f6c6d99d846239c7d508215fa1fb
pabra <pabra@netzfruech.de>
2016-09-18 21:54:21 +0200

    fixed bug in items grouping function

--- manage/build/src_www/js/components/manage_items/index.js
+++ manage/build/src_www/js/components/manage_items/index.js
@@ -4,10 +4,28 @@ import comm from '../../lib/communicator';
 ko.components.register('manage-items', {
     template: require('html!./template.html'),
     viewModel: function () {
-        this.items = ko.observableArray();
+        this.groups = ko.observable({});
+        this.groupsAvailable = ko.observableArray();
+        this.items = ko.observable({});
 
         this.getItems = () => {
-            comm.getItems((data) => this.items(data));
+            comm.getItems((data) => {
+                ko.utils.arrayForEach(data, (v) => {
+                    if (!(v.group_uid in this.groups())) {
+                        this.groups()[v.group_uid] = {
+                            group_uid: v.group_uid,
+                            group_name: v.group_name,
+                            itemsAvailable: ko.observableArray(),
+                        };
+                        this.groupsAvailable.push(v.group_uid);
+                    }
+                    this.groups()[v.group_uid].itemsAvailable.push(v.item_uid);
+                    this.items()[v.item_uid] = {
+                        item_uid: v.item_uid,
+                        item_name: v.item_name,
+                    };
+                });
+            });
         };
     },
 });
--- manage/build/src_www/js/lib/communicator/internal/getItemsFn.js
+++ manage/build/src_www/js/lib/communicator/internal/getItemsFn.js
@@ -6,32 +6,9 @@ const ValueError = errors.ValueError;
 export default function (callback) {
     if ('function' !== typeof callback) throw new ValueError('missing callback function');
 
-    const formatData = function (data) {
-        const groupedData = {};
-        const nestedArrays = [];
-
-        for (let row of data) {
-            if (!(row.group_id in groupedData)) {
-                groupedData[row.group_id] = {
-                    data: row,
-                    items: [],
-                };
-            }
-
-            groupedData[row.group_id].items.push(row);
-        }
-
-        for (const k in groupedData) {
-            if (!groupedData.hasOwnProperty(k)) continue;
-            nestedArrays.push(groupedData[k]);
-        }
-
-        callback(nestedArrays);
-    };
-
     ajax({
         endpoint: '/getItems',
         method: 'get',
-        callback: (data) => formatData(data),
+        callback: (data) => callback(data),
     });
 }
